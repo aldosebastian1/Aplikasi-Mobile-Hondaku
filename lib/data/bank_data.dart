36e9c2f4aeb9d50c9ff0b3a4f6b47c8b6de1d1dd
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-05-07 12:42:46 +0700

    Refactor BankOption class to use logoPath string instead of Widget for improved flexibility

--- lib/data/bank_data.dart
+++ lib/data/bank_data.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class BankOption {
   final String name;
-  final Widget logo;
+  final String logoPath; // Menggunakan path string lebih fleksibel daripada menyimpan Widget langsung
 
-  const BankOption({required this.name, required this.logo});
+  const BankOption({required this.name, required this.logoPath});
 }
 
 // Simulasi database bank agar tidak redundan di setiap halaman
 final List<BankOption> bankOptions = [
   BankOption(
     name: 'BCA Virtual Account',
-    logo: SvgPicture.asset('assets/payments/bca.svg', fit: BoxFit.contain),
+    logoPath: 'assets/payments/bca.svg',
   ),
   BankOption(
     name: 'Mandiri Virtual Account',
-    logo: SvgPicture.asset('assets/payments/mandiri.svg', fit: BoxFit.contain),
+    logoPath: 'assets/payments/mandiri.svg',
   ),
   BankOption(
     name: 'BSI Virtual Account',
-    logo: SvgPicture.asset('assets/payments/bsi.svg', fit: BoxFit.contain),
+    logoPath: 'assets/payments/bsi.svg',
   ),
 ];
