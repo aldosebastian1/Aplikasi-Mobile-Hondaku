b92af82e9c3e425e486294926c32f76b90efbd73
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-23 08:22:41 +0700

    Implementasikan model BankOption dengan metode serialisasi menggunakan JsonSerializable

---
+++ lib/domain/models/bank_option.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bank_option.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_BankOption _$BankOptionFromJson(Map<String, dynamic> json) => _BankOption(
+  name: json['name'] as String,
+  logoPath: json['logoPath'] as String,
+);
+
+Map<String, dynamic> _$BankOptionToJson(_BankOption instance) =>
+    <String, dynamic>{'name': instance.name, 'logoPath': instance.logoPath};
