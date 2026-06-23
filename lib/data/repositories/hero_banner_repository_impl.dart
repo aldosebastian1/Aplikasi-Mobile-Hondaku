8b220c0a18e3f1f879471d94c0be1f093835e0a8
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-23 08:21:11 +0700

    Implementasikan HeroBannerRepositoryImpl untuk mengelola pengambilan data banner hero

---
+++ lib/data/repositories/hero_banner_repository_impl.dart
@@ -0,0 +1,11 @@
+import '../../domain/models/hero_banner.dart';
+import '../../domain/repositories/hero_banner_repository.dart';
+import '../hero_banner_data.dart';
+
+class HeroBannerRepositoryImpl implements HeroBannerRepository {
+  @override
+  Future<List<HeroBanner>> getHeroBanners() async {
+    await Future.delayed(const Duration(milliseconds: 50));
+    return heroBannersDatabase;
+  }
+}
