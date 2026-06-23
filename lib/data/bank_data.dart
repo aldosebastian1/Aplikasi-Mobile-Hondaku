ae4d669c13a006687cc39814264dea163176640a
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-23 08:17:34 +0700

    Perbarui logo bank pada data bank dan hapus kelas BankOption yang tidak terpakai

--- lib/data/bank_data.dart
+++ lib/data/bank_data.dart
@@ -1,24 +1,20 @@
-import 'package:flutter/material.dart';
+import '../domain/models/bank_option.dart';
+export '../domain/models/bank_option.dart';
 
-class BankOption {
-  final String name;
-  final String logoPath; // Menggunakan path string lebih fleksibel daripada menyimpan Widget langsung
 
-  const BankOption({required this.name, required this.logoPath});
-}
 
 // Simulasi database bank agar tidak redundan di setiap halaman
 final List<BankOption> bankOptions = [
   BankOption(
     name: 'BCA Virtual Account',
-    logoPath: 'assets/payments/bca.svg',
+    logoPath: 'assets/payments/bca.png',
   ),
   BankOption(
     name: 'Mandiri Virtual Account',
-    logoPath: 'assets/payments/mandiri.svg',
+    logoPath: 'assets/payments/mandiri.png',
   ),
   BankOption(
     name: 'BSI Virtual Account',
-    logoPath: 'assets/payments/bsi.svg',
+    logoPath: 'assets/payments/bsi.png',
   ),
 ];
