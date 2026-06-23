3b3909b42a378444ff7d3b5f6a3b57d8c8408b25
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-23 07:54:04 +0700

    Perbarui aplikasi untuk menggunakan ProviderScope dan konfigurasi router

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'auth/splash_screen.dart';
+import 'ui/core/router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Hondaku',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFCC0000)),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      routerConfig: goRouter,
     );
   }
 }
