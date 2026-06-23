2ce87f937d2225c896d2f7a94796e6aa8b44dae5
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-23 08:23:32 +0700

    Implementasikan model HeroBanner dengan metode serialisasi menggunakan JsonSerializable

---
+++ lib/domain/models/hero_banner.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hero_banner.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_HeroBanner _$HeroBannerFromJson(Map<String, dynamic> json) => _HeroBanner(
+  image: json['image'] as String,
+  tag: json['tag'] as String,
+  title1: json['title1'] as String,
+  title2: json['title2'] as String,
+  subtitle: json['subtitle'] as String,
+);
+
+Map<String, dynamic> _$HeroBannerToJson(_HeroBanner instance) =>
+    <String, dynamic>{
+      'image': instance.image,
+      'tag': instance.tag,
+      'title1': instance.title1,
+      'title2': instance.title2,
+      'subtitle': instance.subtitle,
+    };
