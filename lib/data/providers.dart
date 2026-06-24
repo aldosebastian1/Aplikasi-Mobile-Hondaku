8ae1ffb2af4f05a7fb3c21ab62a8f7ea3ae43e04
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-24 19:09:02 +0700

    Tambahkan Future Providers untuk Motorcycle, HeroBanner, dan BankOption

--- lib/data/providers.dart
+++ lib/data/providers.dart
@@ -4,6 +4,9 @@ import '../domain/repositories/bank_repository.dart';
 import '../domain/repositories/garage_repository.dart';
 import '../domain/repositories/hero_banner_repository.dart';
 import '../domain/repositories/aktivitas_repository.dart';
+import '../domain/models/motorcycle.dart';
+import '../domain/models/bank_option.dart';
+import '../domain/models/hero_banner.dart';
 import 'repositories/motorcycle_repository_impl.dart';
 import 'repositories/bank_repository_impl.dart';
 import 'repositories/garage_repository_impl.dart';
@@ -29,3 +32,20 @@ final heroBannerRepositoryProvider = Provider<HeroBannerRepository>((ref) {
 final aktivitasRepositoryProvider = Provider<AktivitasRepository>((ref) {
   return AktivitasRepositoryImpl();
 });
+
+// Future Providers for reactive UI consumption
+final motorcyclesProvider = FutureProvider<List<Motorcycle>>((ref) async {
+  final repository = ref.watch(motorcycleRepositoryProvider);
+  return repository.getMotorcycles();
+});
+
+final heroBannersProvider = FutureProvider<List<HeroBanner>>((ref) async {
+  final repository = ref.watch(heroBannerRepositoryProvider);
+  return repository.getHeroBanners();
+});
+
+final bankOptionsProvider = FutureProvider<List<BankOption>>((ref) async {
+  final repository = ref.watch(bankRepositoryProvider);
+  return repository.getBankOptions();
+});
+
