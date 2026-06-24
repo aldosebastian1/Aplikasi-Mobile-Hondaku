d233625e608fb813ab77228f5f998dbd0c65a5ee
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-24 19:04:06 +0700

    Perbarui import di halaman Konfirmasi Pesanan untuk menggunakan model AktivitasItem

--- lib/ui/features/booking/views/konfirmasi_pesanan_page.dart
+++ lib/ui/features/booking/views/konfirmasi_pesanan_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../aktivitas/stores/aktivitas_store.dart';
+import '../../../../domain/models/aktivitas_item.dart';
 import '../../../core/toast/hondaku_toast.dart';
 
 class KonfirmasiPesananPage extends StatefulWidget {
