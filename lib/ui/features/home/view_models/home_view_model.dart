4f06b45d641f6b6600dd3b7fceb66813a62b9f69
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-24 19:04:40 +0700

    Refactor home view model to simplify provider definitions by removing FutureProvider implementations

--- lib/ui/features/home/view_models/home_view_model.dart
+++ lib/ui/features/home/view_models/home_view_model.dart
@@ -1,14 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../data/providers.dart';
-import '../../../../domain/models/motorcycle.dart';
-import '../../../../domain/models/hero_banner.dart';
 
-final homeMotorcyclesProvider = FutureProvider<List<Motorcycle>>((ref) async {
-  final repository = ref.watch(motorcycleRepositoryProvider);
-  return repository.getMotorcycles();
-});
-
-final homeBannersProvider = FutureProvider<List<HeroBanner>>((ref) async {
-  final repository = ref.watch(heroBannerRepositoryProvider);
-  return repository.getHeroBanners();
-});
+final homeMotorcyclesProvider = motorcyclesProvider;
+final homeBannersProvider = heroBannersProvider;
