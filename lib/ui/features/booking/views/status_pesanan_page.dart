650a45eb8bfe5979d594be6cea8675836219b7c0
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-24 19:04:54 +0700

    Remove unused import of activity_store in OrderStatusPage

--- lib/ui/features/booking/views/status_pesanan_page.dart
+++ lib/ui/features/booking/views/status_pesanan_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../domain/models/aktivitas_item.dart';
-import '../../aktivitas/stores/aktivitas_store.dart';
 import '../../aktivitas/view_models/aktivitas_view_model.dart';
 
 
