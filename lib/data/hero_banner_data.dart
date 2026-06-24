1361b2657268cb6f0fd604c8a0aec89b1a11f380
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-06-24 11:02:17 +0700

    Memperbarui ekstensi gambar pada hero banner dari .png ke .jpg

--- lib/data/hero_banner_data.dart
+++ lib/data/hero_banner_data.dart
@@ -6,21 +6,21 @@ export '../domain/models/hero_banner.dart';
 // Simulasi database khusus untuk Hero Banner agar mudah ditambahkan
 final List<HeroBanner> heroBannersDatabase = [
   const HeroBanner(
-    image: 'assets/images/home-hero1.png',
+    image: 'assets/images/home-hero1.jpg',
     tag: 'PRODUK TERBARU',
     title1: 'CBR650R',
     title2: 'Kendalikan Jalanan.',
     subtitle: 'Performa 4-silinder presisi yang dirancang untuk adrenalin murni.',
   ),
   const HeroBanner(
-    image: 'assets/images/home-hero2.png',
+    image: 'assets/images/home-hero2.jpg',
     tag: 'PALING LARIS',
     title1: 'PCX160',
     title2: 'Level Tertinggi.',
     subtitle: 'Rasakan kemewahan berkendara kota dengan kenyamanan tak tertandingi.',
   ),
   const HeroBanner(
-    image: 'assets/images/home-hero3.png',
+    image: 'assets/images/home-hero3.jpg',
     tag: 'PETUALANG',
     title1: 'CRF250 Rally',
     title2: 'Jelajahi Dunia.',
