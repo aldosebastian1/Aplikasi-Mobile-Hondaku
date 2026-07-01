7a16e2a4c634e2e138c0492c95f113f38f13193f
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-01 20:00:46 +0700

    Tambahkan antarmuka FavoriteRepository untuk mengelola favorit sepeda motor

---
+++ lib/domain/repositories/favorite_repository.dart
@@ -0,0 +1,8 @@
+import '../models/motorcycle.dart';
+
+abstract class FavoriteRepository {
+  Future<void> addFavorite(Motorcycle motor);
+  Future<void> removeFavorite(String motorId);
+  Future<bool> isFavorite(String motorId);
+  Future<List<Map<String, dynamic>>> getFavorites();
+}
