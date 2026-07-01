b320e52be8dd1baa340c1d25b73dce247374abb1
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-01 19:59:45 +0700

    Tambahkan UserRepositoryImpl untuk mengelola operasi profil pengguna

---
+++ lib/data/repositories/user_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../domain/models/user_profile.dart';
+import '../../domain/repositories/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final FirebaseFirestore _firestore;
+
+  UserRepositoryImpl({FirebaseFirestore? firestore})
+      : _firestore = firestore ?? FirebaseFirestore.instance;
+
+  @override
+  Future<UserProfile?> getUserProfile(String uid) async {
+    try {
+      final doc = await _firestore.collection('users').doc(uid).get();
+      if (doc.exists && doc.data() != null) {
+        return UserProfile.fromJson(doc.data()!);
+      }
+      return null;
+    } catch (e) {
+      throw Exception('Gagal mengambil data profil dari server: $e');
+    }
+  }
+
+  @override
+  Future<void> saveUserProfile(String uid, UserProfile profile) async {
+    try {
+      await _firestore.collection('users').doc(uid).set(
+        profile.toJson(),
+        SetOptions(merge: true),
+      );
+    } catch (e) {
+      throw Exception('Gagal menyimpan data profil ke server: $e');
+    }
+  }
+}
