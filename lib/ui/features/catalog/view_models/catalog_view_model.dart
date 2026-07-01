d5681b1fd86138dd6874f8d1110005d02020d21c
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-01 20:04:33 +0700

    Menerapkan CatalogFilterState dan CatalogFilterNotifier untuk mengelola filter dan pengurutan katalog

---
+++ lib/ui/features/catalog/view_models/catalog_view_model.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../../domain/models/motorcycle.dart';
+import '../../home/view_models/home_view_model.dart';
+
+class CatalogFilterState {
+  final int selectedFilter;
+  final String activeCategory;
+  
+  const CatalogFilterState({
+    this.selectedFilter = 0,
+    this.activeCategory = '',
+  });
+  
+  CatalogFilterState copyWith({
+    int? selectedFilter,
+    String? activeCategory,
+  }) {
+    return CatalogFilterState(
+      selectedFilter: selectedFilter ?? this.selectedFilter,
+      activeCategory: activeCategory ?? this.activeCategory,
+    );
+  }
+}
+
+class CatalogFilterNotifier extends Notifier<CatalogFilterState> {
+  @override
+  CatalogFilterState build() => const CatalogFilterState();
+
+  void setFilter(int filter) => state = state.copyWith(selectedFilter: filter);
+  void setCategory(String category) => state = state.copyWith(activeCategory: category);
+}
+
+final catalogFilterProvider = NotifierProvider<CatalogFilterNotifier, CatalogFilterState>(CatalogFilterNotifier.new);
+
+double _parsePrice(String priceString) {
+  final cleanString = priceString.replaceAll(RegExp(r'[^0-9]'), '');
+  return double.tryParse(cleanString) ?? 0.0;
+}
+
+final filteredCatalogMotorsProvider = Provider<List<Motorcycle>>((ref) {
+  final motors = ref.watch(homeMotorcyclesProvider).value ?? const [];
+  final filterState = ref.watch(catalogFilterProvider);
+  
+  Iterable<Motorcycle> list = motors;
+  if (filterState.activeCategory.isNotEmpty) {
+    list = list.where((m) => m.categoryBadge.toUpperCase() == filterState.activeCategory.toUpperCase());
+  }
+  
+  final result = list.toList();
+  if (filterState.selectedFilter == 1) {
+    result.sort((a, b) => _parsePrice(a.price).compareTo(_parsePrice(b.price)));
+  } else if (filterState.selectedFilter == 2) {
+    result.sort((a, b) => _parsePrice(b.price).compareTo(_parsePrice(a.price)));
+  }
+  return result;
+});
