ccb7f9bd4566101449ee363e1d98f70e6a197bc9
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-01 19:58:04 +0700

    Ubah fungsi main() untuk menginisialisasi Firebase dan Google Sign-In

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,24 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'l10n/app_localizations.dart';
 import 'ui/core/theme.dart';
 import 'ui/core/router.dart';
+import 'data/seed_data.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await GoogleSignIn.instance.initialize();
+  
+  // UNCOMMENT LATER IF NEEDED: await seedFirestoreDatabase();
+  // We'll let the user run it from a button if they want or just run it here temporarily
+  await seedFirestoreDatabase().then((_) {
+    debugPrint("✅ SEEDING SELESAI");
+  });
+  
   runApp(const ProviderScope(child: MyApp()));
 }
 
