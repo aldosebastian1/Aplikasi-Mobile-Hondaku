75cf741df940a43ead76da9ae6c68e7d7a7d79ed
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-03 17:07:27 +0700

    Hapus impor seed_data yang tidak digunakan dari main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'l10n/app_localizations.dart';
 import 'ui/core/theme.dart';
 import 'ui/core/router.dart';
-import 'data/seed_data.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
