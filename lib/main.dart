1548338ad303fe19717bd4b3f090404d4a81c197
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-03 15:02:24 +0700

    Tambahkan pemuatan variabel lingkungan menggunakan flutter_dotenv di main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,12 @@ import 'ui/core/router.dart';
 import 'data/seed_data.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
