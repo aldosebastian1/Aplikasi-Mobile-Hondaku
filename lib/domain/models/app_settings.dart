7a0c9b7e3eb255957e410983e8a9974d6137ecc2
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-04 21:55:00 +0700

    Hapus dukungan untuk mode gelap pada AppSettings dan perbarui metode copyWith.

--- lib/domain/models/app_settings.dart
+++ lib/domain/models/app_settings.dart
@@ -4,26 +4,22 @@ import 'package:flutter/material.dart';
 class AppSettings {
   final bool notifEnabled;
   final bool biometricEnabled;
-  final bool darkModeEnabled;
   final String selectedLanguage;
 
   const AppSettings({
     required this.notifEnabled,
     required this.biometricEnabled,
-    required this.darkModeEnabled,
     required this.selectedLanguage,
   });
 
   AppSettings copyWith({
     bool? notifEnabled,
     bool? biometricEnabled,
-    bool? darkModeEnabled,
     String? selectedLanguage,
   }) {
     return AppSettings(
       notifEnabled: notifEnabled ?? this.notifEnabled,
       biometricEnabled: biometricEnabled ?? this.biometricEnabled,
-      darkModeEnabled: darkModeEnabled ?? this.darkModeEnabled,
       selectedLanguage: selectedLanguage ?? this.selectedLanguage,
     );
   }
