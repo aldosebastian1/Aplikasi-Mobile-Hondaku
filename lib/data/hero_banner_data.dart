c5388f09fd09f0cf249e56268176faeeb2a392e2
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-05 13:10:56 +0700

    Perbarui jalur gambar untuk hero banner agar sesuai dengan struktur aset terbaru

--- lib/data/hero_banner_data.dart
+++ lib/data/hero_banner_data.dart
@@ -6,21 +6,21 @@ export '../domain/models/hero_banner.dart';
 // Simulasi database khusus untuk Hero Banner agar mudah ditambahkan
 final List<HeroBanner> heroBannersDatabase = [
   const HeroBanner(
-    image: 'assets/images/home-hero1.jpg',
+    image: 'assets/images/banners/home_hero1.jpg',
     tag: 'PRODUK TERBARU',
     title1: 'CBR650R',
     title2: 'Kendalikan Jalanan.',
     subtitle: 'Performa 4-silinder presisi untuk pacu adrenalin di lintasan maupun jalanan kota.',
   ),
   const HeroBanner(
-    image: 'assets/images/home-hero2.jpg',
+    image: 'assets/images/banners/home_hero2.jpg',
     tag: 'PALING LARIS',
     title1: 'PCX160',
     title2: 'Level Tertinggi.',
     subtitle: 'Rasakan kemewahan berkendara di perkotaan dengan kenyamanan tangguh mesin eSP+.',
   ),
   const HeroBanner(
-    image: 'assets/images/home-hero3.jpg',
+    image: 'assets/images/banners/home_hero3.jpg',
     tag: 'PETUALANG',
     title1: 'CRF250',
     title2: 'Jelajahi Dunia.',
