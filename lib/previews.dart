66b73e6a6a15282bddb1f295e5e964306119ff5c
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-05 13:10:42 +0700

    Perbarui aset gambar untuk pratinjau kartu sepeda motor

--- lib/previews.dart
+++ lib/previews.dart
@@ -14,7 +14,7 @@ Widget previewMotorcycleCard() {
     name: 'CBR250RR',
     subtitle: 'Total Control',
     price: 'Rp 62.800.000',
-    imageAsset: 'assets/images/cbr250rr.png',
+    imageAsset: 'assets/images/products/matic/beat.webp',
     description: 'Sensasi berkendara kelas dunia dengan performa mesin 250cc twin silinder.',
     categoryBadge: 'SPORT',
     engine: '250cc, 2-Cylinder',
