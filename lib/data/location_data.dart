3eb79842bdbf7c7722724f22d302e8b958ebb3fb
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-07 19:58:48 +0700

    Tambah data lokasi kecamatan untuk database lokasi

---
+++ lib/data/location_data.dart
@@ -0,0 +1,54 @@
+import '../domain/models/kecamatan.dart';
+
+final List<Kecamatan> locationDatabase = [
+  const Kecamatan(
+    id: 'medan_kota',
+    name: 'Medan Kota',
+    kelurahan: ['Suka Maju', 'Suka Ramai', 'Pandau Hulu', 'Sei Agul', 'Pulo Brayan'],
+  ),
+  const Kecamatan(
+    id: 'medan_baru',
+    name: 'Medan Baru',
+    kelurahan: ['Merdeka', 'Babura', 'Padang Bulan', 'Titi Rantai', 'Tanjung Rejo'],
+  ),
+  const Kecamatan(
+    id: 'medan_petisah',
+    name: 'Medan Petisah',
+    kelurahan: ['Petisah Tengah', 'Sei Putih Barat', 'Sei Putih Timur', 'Sei Sikambing D'],
+  ),
+  const Kecamatan(
+    id: 'medan_sunggal',
+    name: 'Medan Sunggal',
+    kelurahan: ['Sunggal', 'Tanjung Rejo', 'Babura Sunggal', 'Sei Sikambing B'],
+  ),
+  const Kecamatan(
+    id: 'medan_helvetia',
+    name: 'Medan Helvetia',
+    kelurahan: ['Helvetia', 'Cinta Damai', 'Dwikora', 'Tanjung Gusta'],
+  ),
+  const Kecamatan(
+    id: 'medan_johor',
+    name: 'Medan Johor',
+    kelurahan: ['Johor', 'Gedung Johor', 'Kwala Bekala', 'Titi Kuning'],
+  ),
+  const Kecamatan(
+    id: 'medan_amplas',
+    name: 'Medan Amplas',
+    kelurahan: ['Amplas', 'Harjosari I', 'Harjosari II', 'Timbang Deli'],
+  ),
+  const Kecamatan(
+    id: 'medan_denai',
+    name: 'Medan Denai',
+    kelurahan: ['Denai', 'Binjai', 'Tegal Sari Mandala I', 'Tegal Sari Mandala II'],
+  ),
+  const Kecamatan(
+    id: 'medan_tembung',
+    name: 'Medan Tembung',
+    kelurahan: ['Bantan', 'Bantan Timur', 'Sidorejo', 'Sidorejo Hilir'],
+  ),
+  const Kecamatan(
+    id: 'medan_timur',
+    name: 'Medan Timur',
+    kelurahan: ['Glugur Darat I', 'Glugur Darat II', 'Pulo Brayan Darat I', 'Pulo Brayan Darat II'],
+  ),
+];
