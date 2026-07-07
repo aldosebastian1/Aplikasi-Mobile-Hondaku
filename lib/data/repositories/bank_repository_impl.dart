c5706a30902d07114afc9f1829c7ec1c034a3bac
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-07 19:59:35 +0700

    Lakukan refaktorisasi BankRepositoryImpl untuk mengambil opsi bank dari Firestore dan menghapus ketergantungan data lokal

--- lib/data/repositories/bank_repository_impl.dart
+++ lib/data/repositories/bank_repository_impl.dart
@@ -1,10 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../domain/models/bank_option.dart';
 import '../../domain/repositories/bank_repository.dart';
-import '../bank_data.dart';
 
 class BankRepositoryImpl implements BankRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   @override
   Future<List<BankOption>> getBankOptions() async {
-    await Future.delayed(const Duration(milliseconds: 50));
-    return bankOptions;
+    try {
+      final snapshot = await _firestore.collection('banks').get();
+      return snapshot.docs.map((doc) {
+        return BankOption.fromJson(doc.data());
+      }).toList();
+    } catch (e) {
+      throw Exception('Gagal mengambil daftar bank. Pastikan koneksi internet Anda stabil.');
+    }
   }
 }
