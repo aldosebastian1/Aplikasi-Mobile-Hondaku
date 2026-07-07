efdb40641ddc4404d365e034ec1f1099a10e6ed0
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-07 20:00:09 +0700

    Lakukan pemfaktoran ulang HeroBannerRepositoryImpl untuk menghapus ketergantungan data lokal dan menyederhanakan pengambilan data dari Firestore

--- lib/data/repositories/hero_banner_repository_impl.dart
+++ lib/data/repositories/hero_banner_repository_impl.dart
@@ -1,10 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../domain/models/hero_banner.dart';
 import '../../domain/repositories/hero_banner_repository.dart';
-import '../hero_banner_data.dart';
 
 class HeroBannerRepositoryImpl implements HeroBannerRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   @override
   Future<List<HeroBanner>> getHeroBanners() async {
-    await Future.delayed(const Duration(milliseconds: 50));
-    return heroBannersDatabase;
+    try {
+      final snapshot = await _firestore.collection('hero_banners').get();
+      return snapshot.docs.map((doc) {
+        return HeroBanner.fromJson(doc.data());
+      }).toList();
+    } catch (e) {
+      throw Exception('Gagal memuat promo terbaru. Silakan coba lagi.');
+    }
   }
 }
