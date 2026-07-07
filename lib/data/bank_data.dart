cedde30929b3ad4b589d59637243241a6ea3f8f5
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-07 19:58:26 +0700

    Perbarui ID bank options pada data bank

--- lib/data/bank_data.dart
+++ lib/data/bank_data.dart
@@ -1,19 +1,19 @@
 import '../domain/models/bank_option.dart';
 export '../domain/models/bank_option.dart';
 
-
-
-// Simulasi database bank agar tidak redundan di setiap halaman
 final List<BankOption> bankOptions = [
   const BankOption(
+    id: 'bca_va',
     name: 'BCA Virtual Account',
     logoPath: 'assets/payments/bca.png',
   ),
   const BankOption(
+    id: 'mandiri_va',
     name: 'Mandiri Virtual Account',
     logoPath: 'assets/payments/mandiri.png',
   ),
   const BankOption(
+    id: 'bsi_va',
     name: 'BSI Virtual Account',
     logoPath: 'assets/payments/bsi.png',
   ),
