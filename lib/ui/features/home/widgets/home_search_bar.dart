634289a069eb7082566b057223ece13f094ac6d4
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-09 07:52:30 +0700

    Refactor HomeSearchBar untuk menggunakan GestureDetector dan menyederhanakan penanganan input pencarian

--- lib/ui/features/home/widgets/home_search_bar.dart
+++ lib/ui/features/home/widgets/home_search_bar.dart
@@ -2,46 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:hondaku/l10n/app_localizations.dart';
 
 class HomeSearchBar extends StatelessWidget {
-  final TextEditingController controller;
-  final ValueChanged<String> onChanged;
-  final VoidCallback onClear;
-  final String searchQuery;
+  final VoidCallback onTap;
 
   const HomeSearchBar({
     super.key,
-    required this.controller,
-    required this.onChanged,
-    required this.onClear,
-    required this.searchQuery,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 48,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(999),
-        border: Border.all(color: Colors.grey.shade200),
-      ),
-      child: TextField(
-        controller: controller,
-        onChanged: onChanged,
-        textAlignVertical: TextAlignVertical.center,
-        style: const TextStyle(fontSize: 14),
-        decoration: InputDecoration(
-          hintText: AppLocalizations.of(context)!.searchHint,
-          hintStyle: TextStyle(fontSize: 14, color: Colors.grey.shade500),
-          prefixIcon: Icon(Icons.search, size: 18, color: Colors.grey.shade500),
-          suffixIcon: searchQuery.isNotEmpty
-              ? IconButton(
-                  icon: const Icon(Icons.close, size: 16),
-                  onPressed: onClear,
-                )
-              : null,
-          border: InputBorder.none,
-          isDense: true,
-          contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 48,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(999),
+          border: Border.all(color: Colors.grey.shade200),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              Icon(Icons.search, size: 18, color: Colors.grey.shade500),
+              const SizedBox(width: 12),
+              Text(
+                AppLocalizations.of(context)!.searchHint,
+                style: TextStyle(fontSize: 14, color: Colors.grey.shade500),
+              ),
+            ],
+          ),
         ),
       ),
     );
